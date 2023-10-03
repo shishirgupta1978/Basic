9c2a85b7bcc9aa5735d1a1cf5477ed8e3e9fea04
Shishir Gupta <A3238@ind.aptaracorp.com>
2023-10-03 17:19:25 +0530

    updated

--- frontend/src/Urls.jsx
+++ frontend/src/Urls.jsx
@@ -16,39 +16,26 @@ export const Urls=()=> {
       
       
       <Routes>
-        
-        <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage/>}/>  
-        <Route path="create-website" element={<PrivateRoute><CreateWebsite/></PrivateRoute>}>
-        <Route index element={<PrivateRoute><WebsiteProfile/></PrivateRoute>}/>  
-        <Route path="update-categories" element={<UpdateCategories/>}/>
-        <Route path="update-products" element={<UpdateProducts/>}/>
-        <Route path="website" element={<ChangePassword/>}/>
-        </Route>
-
-        
-        
-
-</Route>
-<Route path="website/:uid" element={<Website />}>
-   
-        
-        
-
-        
-   <Route index element={ <Products/>}/>
-   <Route path="cart" element={<Cart/>}/>
-   <Route path="login" element={<Login/>}/>
+        <Route path="/" element={<Navigate to="/webmaster/" />}/>
+        <Route path="/:website/" element={<Layout />}>
+        <Route index element={ <Products/>}/>
+        <Route path="cart" element={<Cart/>}/>
+        <Route path="login" element={<Login/>}/>
         <Route path="forgetpassword" element={<ForgetPassword/>}/>
         <Route path="register" element={<Register/>}/>
         <Route path="activate/:uid/:token" element={<ActivateUser/>}/>
         <Route path="resetpassword" element={<ResetPassword/>}/>
         <Route path="profile" element={<PrivateRoute><UpdateProfile/></PrivateRoute>}/>
         <Route path="changepassword" element={<PrivateRoute><ChangePassword/></PrivateRoute>}/>
-   
+        <Route index element={<HomePage/>}/>  
+        <Route path="create-website" element={<PrivateRoute><CreateWebsite/></PrivateRoute>}>
+          <Route index element={<PrivateRoute><WebsiteProfile/></PrivateRoute>}/>  
+          <Route path="update-categories" element={<UpdateCategories/>}/>
+          <Route path="update-products" element={<UpdateProducts/>}/>
+          <Route path="website" element={<ChangePassword/>}/>
+        </Route>
 
-     </Route>
-
+</Route>
 
        </Routes>
       <ToastContainer theme="dark" />
